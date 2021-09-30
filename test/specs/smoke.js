dd04a0057560f47145c573a8a39b9bcee362df77
Nata Lu <lavazka@gmail.com>
2021-09-30 20:25:58 +0300

    pause added

--- test/specs/smoke.js
+++ test/specs/smoke.js
@@ -2,6 +2,7 @@ describe('Website', function() {
     it('should be alive', function() {
         browser.url('/')
         const img = $('img[src="https://static.litecart.net/storage/images/logotype_light.svg"]')
+        browser.pause(10000)
         if(!img.isExisting()){
             throw new Error ('Website should be opened, and logo displayed');
         }
